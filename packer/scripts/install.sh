db92db1e5b5bec507191f285d81d840548c8e9d5
Jonny Heywood <jonathandheywood@gmail.com>
2021-03-02 10:48:49 +0000

    updated script

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-"echo 'packer' | sudo su"
+echo 'packer' | sudo su
 #The above command will switch to root, and hopefully prevent the install failing. 
 set -uex
 #Line 2 means if the scripts fails then packer fails, and provide detailed exception message. 
