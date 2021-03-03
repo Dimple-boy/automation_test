58f7e4e4c912c6ac46a4e51d200c35d6d28c8db3
Jonny Heywood <jonathandheywood@gmail.com>
2021-03-03 10:18:40 +0000

    rejigged packer script

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'packer' | sudo su
-#The above command will switch to root, and hopefully prevent the install failing. 
 set -uex
 #Line 2 means if the scripts fails then packer fails, and provide detailed exception message. 
+"echo 'packer' | sudo su"
+#The above command will switch to root, and hopefully prevent the install failing. 
 apt update -y
 apt install nginx -y
 ufw allow 'Nginx HTTP'
